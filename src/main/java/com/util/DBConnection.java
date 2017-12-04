ffb65a79055a887134d194fed273f899b110e384
Jeet Khatri <jeet.g.khatri@gmail.com>
2017-12-04 17:49:35 +0530

    Update DBConnection.java

--- src/main/java/com/util/DBConnection.java
+++ src/main/java/com/util/DBConnection.java
@@ -10,8 +10,9 @@ public class DBConnection {
 	public static Connection getConnection() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver"); 
-			Connection conn =DriverManager.getConnection("jdbc:mysql://mysql:3306/sampledb","user","password");
+			//Connection conn =DriverManager.getConnection("jdbc:mysql://mysql:3306/sampledb","user","password");
             //Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/photography", "root", "root");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://65.19.141.67:3306/sharvil_fotocourtesy","sharvil_wp614","urvilsh@H1628");
 			if (conn != null)
 			{
 				
