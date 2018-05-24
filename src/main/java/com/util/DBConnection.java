b9233907f3d205d44ae0a284c53f4ae38751dca6
Sharvil Shah <sharvilshah1996@gmail.com>
2018-05-24 22:38:52 +0530

    Update DBConnection.java

--- src/main/java/com/util/DBConnection.java
+++ src/main/java/com/util/DBConnection.java
@@ -14,7 +14,7 @@ public class DBConnection {
             //Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/photography", "root", "root");
 			//Connection conn = DriverManager.getConnection("jdbc:mysql://sql12.freemysqlhosting.net:3306/sql12230123","sql12230123","9Y5VZmlPqn");
 			
-			Connection conn = DriverManager.getConnection("jdbc:mysql://sql12.freesqldatabase.com:3306/sql12239684","sql12239684","AS1pZyD6HE")
+			Connection conn = DriverManager.getConnection("jdbc:mysql://sql12.freesqldatabase.com:3306/sql12239684","sql12239684","AS1pZyD6HE");
 			if (conn != null)
 			{
 			
