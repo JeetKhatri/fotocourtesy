7a49e321b7d7af8f8c2a2faef0fabef2a0be3328
Sharvil Shah <sharvilshah1996@gmail.com>
2018-05-24 22:37:49 +0530

    Update DBConnection.java

--- src/main/java/com/util/DBConnection.java
+++ src/main/java/com/util/DBConnection.java
@@ -13,12 +13,11 @@ public class DBConnection {
 			//Connection conn =DriverManager.getConnection("jdbc:mysql://mysql:3306/sampledb","user","password");
             //Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/photography", "root", "root");
 			//Connection conn = DriverManager.getConnection("jdbc:mysql://sql12.freemysqlhosting.net:3306/sql12230123","sql12230123","9Y5VZmlPqn");
-			//Connection conn = DriverManager.getConnection("jdbc:mysql://65.19.141.67:3306/sharvil_fotocourtesy","sharvil_wp614","urvilsh@H1628");
-			Connection conn = DriverManager
-					.getConnection("jdbc:mysql://db4free.net:3306/sharvilshah",
-					"sharvil","urvilsh@H1628");
+			
+			Connection conn = DriverManager.getConnection("jdbc:mysql://sql12.freesqldatabase.com:3306/sql12239684","sql12239684","AS1pZyD6HE")
 			if (conn != null)
 			{
+			
 				
 				return conn;
 			}
